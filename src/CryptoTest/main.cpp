e6fd143de9a04def7f7d1f9f88a3c11fe5f123e3
Brandon Lehmann <brandonlehmann@gmail.com>
2018-08-11 16:19:45 -0400

    Introducing... cn_soft_shell

--- src/CryptoTest/main.cpp
+++ src/CryptoTest/main.cpp
@@ -28,21 +28,54 @@ int main(int argc, char** argv) {
     Hash hash = Hash();
 
     cn_fast_hash(rawData.data(), rawData.size(), hash);
-    std::cout << "Cryptonight Fast Hash: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+    std::cout << "cn_fast_hash: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
 
     cn_slow_hash_v6(rawData.data(), rawData.size(), hash);
-    std::cout << "Cryptonight v0: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+    std::cout << "cn_slow_hash_v0: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
 
     if (rawData.size() >= 43)
     {
       cn_slow_hash_v7(rawData.data(), rawData.size(), hash);
-      std::cout << "Cryptonight v1: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      std::cout << "cn_slow_hash_v1: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
 
       cn_lite_slow_hash_v0(rawData.data(), rawData.size(), hash);
-      std::cout << "Cryptonight Lite v0: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      std::cout << "cn_lite_slow_hash_v0: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
 
       cn_lite_slow_hash_v1(rawData.data(), rawData.size(), hash);
-      std::cout << "Cryptonight Lite v1: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      std::cout << "cn_lite_slow_hash_v1: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 0);
+      std::cout << "cn_soft_shell(0): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 512);
+      std::cout << "cn_soft_shell(512): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 1024);
+      std::cout << "cn_soft_shell(1024): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 1536);
+      std::cout << "cn_soft_shell(1536): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 2048);
+      std::cout << "cn_soft_shell(2048): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 2560);
+      std::cout << "cn_soft_shell(2560): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 3072);
+      std::cout << "cn_soft_shell(3072): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 3584);
+      std::cout << "cn_soft_shell(3584): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 4096);
+      std::cout << "cn_soft_shell(4096): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 6144);
+      std::cout << "cn_soft_shell(6144): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      
+      cn_soft_shell(rawData.data(), rawData.size(), hash, 8192);
+      std::cout << "cn_soft_shell(8192): " << Common::toHex(&hash, sizeof(Hash)) << "\n";
     }
   }
   catch (std::exception& e)
